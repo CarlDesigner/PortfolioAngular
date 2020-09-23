9c17045c87162ac93982cd0637f5f0ff84538d36
CarlDesigner <krlitosfajardo626@gmail.com>
2020-09-23 01:43:29 -0500

    proyecto con loading SVG Listo

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,7 +9,7 @@ import { Producto } from '../interfaces/producto.interface';
 export class ProductosService {
 
   cargando = true;
-
+  productos : Producto[] = [];
 
     constructor( private http: HttpClient  ) {
 
@@ -24,8 +24,11 @@ export class ProductosService {
               .subscribe(  (resp: Producto[]) => {
 
                   console.log(resp);
+                  this.productos = resp;
 
-                  this.cargando = false;
+                  setTimeout(() => {
+                    this.cargando = false;
+                }, 200);
 
               } );
 
